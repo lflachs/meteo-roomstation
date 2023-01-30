1fd20aaaed4699784912e5f1d32c6854be91d23a
Leonard Flachs <leonard.flachs@leanovate.de>
2023-01-30 23:34:25 +0100

    Add now route and log at start

--- app/index.js
+++ app/index.js
@@ -1,16 +1,22 @@
-const sensor = require('./sensor')
-const express = require('express');
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient()
-
+const sensor = require("./sensor");
+const express = require("express");
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
 
 const app = express();
 
-app.get('/meteo-data', async(req, res) => {
-    const meteoData = await prisma.meteoData.findMany();
-    res.json(meteoData)
-})
+app.get("/data", async (req, res) => {
+  const meteoData = await prisma.meteoData.findMany();
+  res.json(meteoData);
+});
+
+app.get("/data/now", (req, res) => {
+  const data = sensor.getData();
+  res.json(data);
+});
 
 app.listen(3000, () => {
-    console.log(`App listening on port 3000`)
-})
+  console.log(`App listening on port 3000`);
+  const data = sensor.getData();
+  console.log(data);
+});
